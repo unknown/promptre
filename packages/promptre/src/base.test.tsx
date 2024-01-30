6674fac5b833871d6ecf22999b5bbf2deea766d8
David Mo <44306450+unknown@users.noreply.github.com>
2024-01-30 17:25:55 -0500

    Respect token limits in rendering

--- packages/promptre/src/base.test.tsx
+++ packages/promptre/src/base.test.tsx
@@ -1,17 +1,27 @@
 import * as Promptre from ".";
+import { encode } from "@promptre/tokenizer";
 
 import { expect, test } from "vitest";
 
-test("scope renders all of its children", () => {
-  const scopeTest = (
-    <scope>
-      <scope>Scope 1</scope>
-      <scope>
-        Text
-        <scope>Scope 2</scope>
-        <scope>Scope 3</scope>
-      </scope>
+const scopeTest = (
+  <scope p={1000}>
+    <scope prel={2}>Scope 1</scope>
+    <scope prel={10}>
+      Text
+      <scope prel={1}>Scope 2</scope>
+      <scope prel={2}>Scope 3</scope>
     </scope>
-  );
+  </scope>
+);
+
+test("scope renders all of its children", () => {
   expect(Promptre.render(scopeTest)).toBe("Scope 1 Text Scope 2 Scope 3");
+});
+
+test("rendering scopes respects token limit", () => {
+  for (let tokenLimit = 0; tokenLimit <= 10; ++tokenLimit) {
+    const prompt = Promptre.render(scopeTest, { tokenLimit });
+    const tokens = encode(prompt);
+    expect(tokens).lte(tokenLimit);
+  }
 });
